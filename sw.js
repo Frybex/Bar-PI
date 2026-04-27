680983ffc3023e326dbddad7a61441b17621691e
Frybex <maxence.bastin@gmail.com>
2026-04-27 03:28:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const CACHE = 'bar-scout-v1';
-const ASSETS = [
+const CACHE = 'bar-scout-v2';
+const CORE_ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
+];
+const OPTIONAL_ASSETS = [
   'https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600&family=DM+Mono:wght@400;500&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/firebase/9.23.0/firebase-app-compat.min.js',
@@ -14,7 +16,15 @@ const ASSETS = [
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE).then(async c => {
+      // Les assets locaux DOIVENT être en cache
+      await c.addAll(CORE_ASSETS);
+      // Les CDN externes : on tolère les échecs pour ne pas bloquer l'install
+      await Promise.all(OPTIONAL_ASSETS.map(url =>
+        c.add(url).catch(err => console.warn('SW: skip cache', url, err))
+      ));
+      return self.skipWaiting();
+    })
   );
 });
 
